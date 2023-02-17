c8e0405f8e377ae902c8b0bef62ba98f575f7031
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2023-02-17 11:08:42 +0600

    updatecheck

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("CRUD Server Running");
+  res.send("CRUD Server Running Successfully");
 });
 
 app.listen(port, () => {
